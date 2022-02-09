39ffcb91f610e3df624d960796d4dc0034377ed3
tmdrl980430 <tmdrl980430@naver.com>
2022-02-09 11:31:38 +0900

    frontone start

--- 5week/movie_app/src/App.js
+++ 5week/movie_app/src/App.js
@@ -1,6 +1,28 @@
+import React, { useEffect, useState } from "react";
 
-
-function App() {
+const App = () => {
+    //State는 절대 직접적으로 수정하지 않는다.
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (e) => setToDo(e.target.value);
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if(toDo === ""){
+            return;
+        }
+        setToDo("");
+        console.log(toDo);
+        toDo
+    }
+    
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do,,," />
+                <button>Add To Do</button>
+            </form>
+        </div>
+    )
 }
 
 export default App;
