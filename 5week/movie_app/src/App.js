ce65a994b7954f13c669312b401094f59b067b79
Duke(김승기) <74481440+tmdrl980430@users.noreply.github.com>
2022-03-01 18:14:44 +0900

    react-dom-link

--- 5week/movie_app/src/App.js
+++ 5week/movie_app/src/App.js
@@ -1,10 +1,14 @@
 import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
 import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 //router에는 BrowserRouter와 HashRouter가 있다.
 const App = () => {
     return (
     <Router>
+        <Routes>
+            <Route exact path="/movie" element={<Detail/> }/>
+        </Routes>
         <Routes>
             <Route exact path="/" element={<Home/>} />
         </Routes>
